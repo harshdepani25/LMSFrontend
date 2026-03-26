45a3baeffe4a2b82f227f96489eb489b953b5243
harshdepani25 <harshdepani2509@gmail.com>
2026-03-26 21:30:17 +0530

    fd

--- src/container/Category/Category.jsx
+++ src/container/Category/Category.jsx
@@ -14,40 +14,16 @@ function Category() {
         aria-labelledby="course-pills-tab-1"
       >
         <div className="row g-4">
-             <div className="col-sm-6 col-lg-4 col-xl-3">
-              <div className="card shadow h-100">
-                {/* Image */}
-                <img
-                  src={v.category_img.url}
-                  className="card-img-top"
-                  alt="course image"
-                />
-                {/* Card body */}
-                <div className="card-body pb-0">
-                  {/* Badge and favorite */}
-                  <div className="d-flex justify-content-between mb-2"></div>
-                  {/* Title */}
-                  <h5 className="card-title fw-normal">
-                    <a href="#">{v.name}</a>
-                  </h5>
-                  <p className="mb-2 text-truncate-2">{v.desciption}</p>
-                </div>
-              </div>
-            </div>
           {/* {cateData.map((v) => (
             <div className="col-sm-6 col-lg-4 col-xl-3">
               <div className="card shadow h-100">
-                {/* Image */}
                 <img
                   src={v.category_img.url}
                   className="card-img-top"
                   alt="course image"
                 />
-                {/* Card body */}
                 <div className="card-body pb-0">
-                  {/* Badge and favorite */}
                   <div className="d-flex justify-content-between mb-2"></div>
-                  {/* Title */}
                   <h5 className="card-title fw-normal">
                     <a href="#">{v.name}</a>
                   </h5>
