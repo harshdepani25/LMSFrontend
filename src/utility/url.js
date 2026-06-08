9843eb5f6a23f990be17253d2f6bcf6641694613
harshdepani25 <harshdepani2509@gmail.com>
2026-06-08 16:37:28 +0530

    url

--- src/utility/url.js
+++ src/utility/url.js
@@ -1,4 +1,4 @@
-export const BASE_URL="https://lms-backend-ed6k.vercel.app//api/v2/";
+export const BASE_URL="https://lms-backend-ed6k.vercel.app/api/v2/";
 export const IMAGE_URL="https://lms-backend-ed6k.vercel.app/";
 
 // export const BASE_URL="http://localhost:3030/api/v2/";
