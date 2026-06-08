264934006fd37ca09ffe1d8c2c5f1619e525027b
harshdepani25 <harshdepani2509@gmail.com>
2026-06-08 16:35:42 +0530

    new url

--- src/utility/url.js
+++ src/utility/url.js
@@ -1,5 +1,5 @@
-export const BASE_URL="https://lms-backend-topaz-eight-98.vercel.app/api/v2/";
-export const IMAGE_URL="https://lms-backend-topaz-eight-98.vercel.app/";
+export const BASE_URL="https://lms-backend-ed6k.vercel.app//api/v2/";
+export const IMAGE_URL="https://lms-backend-ed6k.vercel.app/";
 
 // export const BASE_URL="http://localhost:3030/api/v2/";
 // export const IMAGE_URL="http://localhost:3030/"
